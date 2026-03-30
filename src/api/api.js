4950ed731759db04dcea7a47a9a4bdd7c47a66cc
thatipallyvinodkumar167 <thatipallyvinodkumar167@gmail.com>
2026-03-30 10:11:01 +0530

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5001/api/v1";
-export const API_ROOT_URL = "http://localhost:5001";
+const DEFAULT_API_ROOT_URL = "http://localhost:5001";
+const DEFAULT_API_BASE_URL = `${DEFAULT_API_ROOT_URL}/api/v1`;
+
+export const API_ROOT_URL =
+  process.env.REACT_APP_API_ROOT_URL || DEFAULT_API_ROOT_URL;
+export const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || DEFAULT_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
